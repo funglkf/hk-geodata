91a01a395e495132ed7afd0e190de89abee86991
FunG <funglkf@outlook.com>
2022-05-02 16:50:40 +0800

    capture json

--- get_hkget-data.py
+++ get_hkget-data.py
@@ -19,17 +19,47 @@ soup = BeautifulSoup(response.text, 'html.parser')
 ## Find all script content
 script_list = soup.find_all('script')
 
-## Get g_gsDatasetInfo data to Json object
+## Get DatasetInfo data to Json object
 for script in script_list:
   if 'var g_gsDatasetInfo' in str(script):
-    
+    # print(str(script))
     m = re.search(r'var g_gsDatasetInfo = (.+?);', str(script), re.DOTALL)
-    m.group(1)
-    json_data = json.loads(m.group(1))
+    open('docs/g_gsDatasetInfo.json', 'a').write(m.group(1))
+    g_gsDatasetInfo_json = json.loads(m.group(1))
 
-df_item = pd.DataFrame.from_records(json_data)
+  if 'var g_hkgsCategoryInfo' in str(script):
+    m = re.search(r'var g_hkgsCategoryInfo = (.+?);', str(script), re.DOTALL)
+    open('docs/g_hkgsCategoryInfo.json', 'a').write(m.group(1))
+    g_hkgsCategoryInfo_json = json.loads(m.group(1))
 
+  if 'var hkgsDataProviderList' in str(script):
+    m = re.search(r'var hkgsDataProviderList = (\[.+?\])', str(script), re.DOTALL)
+    open('docs/hkgsDataProviderList.json', 'a').write(m.group(1))
+    hkgsDataProviderList_json = json.loads(m.group(1))
+
+  if 'var hkgsDataProviderAndDatasetMapping' in str(script):
+    m = re.search(r'var hkgsDataProviderAndDatasetMapping = ({.+?})', str(script), re.DOTALL)
+    open('docs/hkgsDataProviderAndDatasetMapping.json', 'a').write(m.group(1))
+    hkgsDataProviderAndDatasetMapping_json = json.loads(m.group(1))
+
+  if 'var hkgsCategoryList' in str(script):
+    m = re.search(r'var hkgsCategoryList = (.+?);', str(script), re.DOTALL)
+    open('docs/hkgsCategoryList.json', 'a').write(m.group(1))
+    hkgsCategoryList_json = json.loads(m.group(1))
+
+
+## Changing Json object to DataFrame
+df_g_gsDatasetInfo = pd.DataFrame.from_records(g_gsDatasetInfo_json)
+df_hkgsDataProviderList = pd.DataFrame.from_records(hkgsDataProviderList_json)
+df_hkgsCategoryList= pd.DataFrame.from_records(hkgsCategoryList_json)
+# df_hkgsDataProviderAndDatasetMapping = pd.DataFrame.from_records(hkgsDataProviderAndDatasetMapping_json)
+# df_g_hkgsCategoryInfo = pd.DataFrame.from_records(g_hkgsCategoryInfo_json)
+
+
+## Mapping Provider Name
+df_maindata = pd.merge(df_g_gsDatasetInfo[['DATASET_UUID','DATASET_NAME_EN','DATASET_NAME_TC','DATA_PROVIDER_ID']],
+         df_hkgsDataProviderList[['dpid','nameEN','nameTC']], left_on='DATA_PROVIDER_ID', right_on='dpid').drop(columns=['dpid', 'DATA_PROVIDER_ID'])
 
 ## write to index page
 with open('docs/index.html', 'w') as fo:
-    df_item.to_html(fo)
+    df_maindata.to_html(fo)
