706b34615906c6139c24342cd0d7d9b3cecbc0d5
FunG <funglkf@outlook.com>
2022-05-09 04:11:10 +0800

    add map

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,10 +21,13 @@
         action: function (e, row) {
           window
             .open(
-              `https://geojson.tools/?url=https://geodata.gov.hk/gs/api/v1.0.0/geoDataQuery?q=%7Bv%3A%221%2E0%2E0%22%2Cid%3A%22` +
-                row.getData()["DATASET_UUID"] +
-                `%22%2Clang%3A%22ALL%22%7D`,
-              "_blank"
+              "/map/" +
+                row.getData()["DATASET_NAME_EN"].replace(/[^\w_-]/g, "_"),
+              "_self"
+              // `https://geojson.tools/?url=https://geodata.gov.hk/gs/api/v1.0.0/geoDataQuery?q=%7Bv%3A%221%2E0%2E0%22%2Cid%3A%22` +
+              //   row.getData()["DATASET_UUID"] +
+              //   `%22%2Clang%3A%22ALL%22%7D`
+              // ,"_blank"
             )
             .focus();
         },
---
+++ src/routes/map/[item].svelte
@@ -0,0 +1,31 @@
+<script context="module">
+  export const prerender = true;
+  export async function load({ fetch, params }) {
+    // let post;
+    // try {
+    //   // here we are gonna fetch the single article by id
+    //   post = await fetch(`/${params.item}`);
+    //   post = await post.json();
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    return {
+      props: {
+        apiUrl: "/json/" + params.item + ".geojson",
+        item: params.item,
+      },
+    };
+  }
+</script>
+
+<script>
+  import LeafletMap from "$lib/LeafletMap.svelte";
+
+  export let apiUrl;
+  export let item;
+</script>
+
+<h1 class="px-6 text-lg font-semibold">Map ({item.replace(/[_]/g, " ")})</h1>
+<div class="mx-5">
+  <LeafletMap {apiUrl} />
+</div>
